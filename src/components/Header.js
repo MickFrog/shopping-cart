62f07f52d32a6689d71caad24a8b6fe984a04bfb
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-05-29 17:01:38 +0300

    add header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from "react";
 import './Header.css';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="myHeader">
-            MICK STORE
+            <div className="storeName">
+                <Link to='/'>
+                    <h1>Mick-Store</h1>
+                </Link>
+            </div>
+
+            <div className="nav-links">
+                <Link to='/shop'>
+                    <div>Shop</div>
+                </Link>
+
+                <Link to='/cart'>
+                    <div>Cart</div>
+                </Link>
+            </div>
         </div>
     )
 
