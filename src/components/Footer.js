b4f81ad5ecddf4d42592d6877415ee20116fe556
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-05 13:31:45 +0300

    add text content to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,20 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="myFooter">
-            footer is posed to be here
+            <div className="about-details">
+                <div className="address">
+                    <h4>Our Headquarters</h4>
+                    <p>1234 Main Street</p>
+                    <p>Cityville, Stateville 56789</p>
+                    <p>United States</p>
+                </div>
+
+                <div>
+                    <h4>Follow us on our socials</h4>
+                </div>
+            </div>    
+                    
+            <div>Copyright &copy; 2023 <a href="https://www.github.com/MickFrog" rel="noreferrer" target="_blank">&nbsp;MickFrog</a></div>
         </div>
     )
 
