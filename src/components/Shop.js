12eb1a157344cae766e9eada639b57a6c5d6d67c
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-07 19:36:08 +0300

    add json file for products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,13 @@
 import React from "react";
 import './Shop.css';
+import productsData from './shopComponents/products.json'
+
+function getProducts() {
+    console.table(productsData.groceries);
+}
 
 const Shop = () => {
+    getProducts();
     return (
         <div className="contentBg shop-container">
             My Shop
