6b0c206ce9255fb2a31463dd3fd7d5f517845e31
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-06-09 11:42:17 +0300

    implement cart

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,6 +11,47 @@ import products from './products.json';
 const stock = products.groceries;
 
 const RouteSwitch = () => {
+    const [cart, setCart] = useState([]);
+    /*Format: cart = [{
+    *     cartPdt: {pdt},
+    *     quantity: number eg 1
+    *    }]
+    */
+   
+    const updateCart = useCallback((pdt, bAction) => { //bAction ie 1-add or (-1)-remove
+        const productInCart = cart.filter((elem) => elem.cartPdt === pdt);
+        
+        if(productInCart) {
+            let updatedCart = [];
+            let elemRemoved = false;
+
+            updatedCart = cart.map((elem) => {
+                if(elem.cartPdt === pdt) {
+                    elem.quantity += bAction;
+                    
+                    if(elem.quantity < 1) {
+                        elemRemoved = true;
+                        return undefined;
+                    }
+                }
+                return elem;
+            })
+
+            if(elemRemoved) { //filter out removed cart Elements
+                updatedCart = updateCart.filter((currElem) => currElem !== undefined)
+            }
+
+            setCart(updatedCart)
+            return;
+        }
+
+        //Product not in cart
+        setCart([...cart, {
+            cartPdt: pdt,
+            quantity: 1
+        }]);
+    }, [cart])
+
     return (
         <BrowserRouter>
             <Header />
