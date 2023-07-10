ed8614e15284ec4b45f0bbc898baded0a7d477ff
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-07-10 15:35:49 +0300

    add responsiveness to shop page

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import App from './App';
+import App from "./App";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
-import products from './products.json';
+import products from "./products.json";
 
 //load products from JSON file
 const stock = products.groceries;
@@ -14,70 +14,88 @@ const stock = products.groceries;
 const RouteSwitch = () => {
     const [cart, setCart] = useState([]);
     /*Format: cart = [{
-    *     cartPdt: {pdt},
-    *     quantity: number eg 1
-    *    },...]
-    */
+     *     cartPdt: {pdt},
+     *     quantity: number eg 1
+     *    },...]
+     */
 
-    const updateCart = useCallback((pdt, bAction) => { //bAction ie 1-add or (-1)-remove
-        const productInCart = cart.filter((elem) => elem.cartPdt === pdt);
-        
-        if(productInCart.length > 0) {
-            let updatedCart = [];
-            let elemRemoved = false;
+    const updateCart = useCallback(
+        (pdt, bAction) => {
+            //bAction ie 1-add or (-1)-remove
+            const productInCart = cart.filter((elem) => elem.cartPdt === pdt);
 
-            updatedCart = cart.map((elem) => { //modify cart
-                if(elem.cartPdt === pdt) {
-                    elem.quantity += bAction;
-                    
-                    if(elem.quantity < 1) {
-                        elemRemoved = true;
-                        return undefined;
+            if (productInCart.length > 0) {
+                let updatedCart = [];
+                let elemRemoved = false;
+
+                updatedCart = cart.map((elem) => {
+                    //modify cart
+                    if (elem.cartPdt === pdt) {
+                        elem.quantity += bAction;
+
+                        if (elem.quantity < 1) {
+                            elemRemoved = true;
+                            return undefined;
+                        }
                     }
-                }
-                return elem;
-            })
+                    return elem;
+                });
 
-            if(elemRemoved) { //filter out removed cart Elements
-                updatedCart = updatedCart.filter((currElem) => currElem !== undefined)
+                if (elemRemoved) {
+                    //filter out removed cart Elements
+                    updatedCart = updatedCart.filter(
+                        (currElem) => currElem !== undefined
+                    );
+                }
+
+                setCart(updatedCart);
+                return;
             }
 
-            setCart(updatedCart)
-            return;
-        }
-
-        //Product not in cart
-        setCart([...cart, {
-            cartPdt: pdt,
-            quantity: 1
-        }]);
-    }, [cart])
+            //Product not in cart
+            setCart([
+                ...cart,
+                {
+                    cartPdt: pdt,
+                    quantity: 1,
+                },
+            ]);
+        },
+        [cart]
+    );
 
     const [cartCount, setCartCount] = useState(0);
 
-    useEffect(() => { //update cartCount onChange of cart
-        if(cart.length < 1) return; //prevent running on empty cart
+    useEffect(() => {
+        //update cartCount onChange of cart
+        if (cart.length < 1) return; //prevent running on empty cart
 
         let newCount = cart.reduce((total, elem) => {
-            return total + elem.quantity
+            return total + elem.quantity;
         }, 0);
 
         setCartCount(newCount);
-    }, [cart])
+    }, [cart]);
 
     return (
         <HashRouter>
-            <Header cartCount={cartCount}/>
+            <Header cartCount={cartCount} />
 
             <Routes>
-                <Route path="/" element={<App />}/>
-                <Route path="/shop" element={<Shop items={stock} cartCallback={updateCart}/>}/>
-                <Route path="/cart" element={<Cart cartData={cart} cartCallback={updateCart}/>}/>
+                <Route path="/" element={<App />} />
+                <Route
+                    path="/shop"
+                    element={<Shop items={stock} cartCallback={updateCart} />}
+                />
+                <Route
+                    path="/cart"
+                    element={<Cart cartData={cart} cartCallback={updateCart} />}
+                />
             </Routes>
 
             <Footer />
         </HashRouter>
-    )
-}
+    );
+};
 
 export default RouteSwitch;
--- src/components/shopComponents/product.js
+++ src/components/shopComponents/product.js
@@ -1,21 +1,26 @@
 import React from "react";
-import './product.css';
+import "./product.css";
 
 const Product = (props) => {
-
     const addCart = () => {
         props.cartCallback(props.productDetails, 1);
-    }
+    };
 
     return (
         <div className="pdt-container">
-            <img className="pdt-image" src={props.productDetails.imageSrc} alt={props.productDetails.product} />
+            <img
+                className="pdt-image"
+                src={props.productDetails.imageSrc}
+                alt={props.productDetails.product}
+            />
 
             <div className="pdt-name">{props.productDetails.product}</div>
             <div className="pdt-price">{"$" + props.productDetails.price}</div>
-            <button className="addCart-Btn" onClick={addCart}>Add to cart</button>
+            <button className="addCart-Btn" onClick={addCart}>
+                Add to cart
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Product;
